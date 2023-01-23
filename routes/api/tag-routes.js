8e2dbca23e3c654dd21ed91caf0c6da7f182c0db
Eric Marcy <ericmarcypro@gmail.com>
2023-01-23 12:30:45 -0500

    added getOne and post routes for tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,11 +19,40 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  try {
+    Tag.findOne({where: {id: req.params.id}}, { include : [Product, {
+      through: ProductTag,
+      model: Tag,
+    }]})
+      .then(tags => res.json(tags))
+  } catch(err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((tag) => {
+    // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+    if (req.body.tagIds.length) {
+      const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: tag.id,
+          tag_id,
+        };
+      });
+      return ProductTag.bulkCreate(productTagIdArr);
+    }
+    // if no product tags, just respond
+    res.status(200).json(tag);
+  })
+  .then((productTagIds) => res.status(200).json(productTagIds))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
